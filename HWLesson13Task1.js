00289753d98a6f61f4b6c33af1c0d54edb905ecb
Alexsey Nesterov <nesterov.alexsey@gmail.com>
2023-05-22 15:57:32 +0200

    created draft with short time and only name

--- HWLesson13Task1.js
+++ HWLesson13Task1.js
@@ -3,3 +3,23 @@
 // объекта (студент с именем "Anri" и возрастом 32), c задержкой 4 секунды.
 // Вызовите этот метод - сохраните в переменную.
 // Раскройте promise при помощи then, выведите имя студента.
+
+class Student {
+    constructor(studentName, age) {
+        this.studentName = studentName;
+        this.age = age;
+    }
+}
+
+function getStudent() {
+    return new Promise(function(resolve, reject){
+        setTimeout(() => {
+            resolve("Anri");
+        }, 1000)
+    })
+}
+
+let studentAnri = getStudent();
+studentAnri.then((value) => {
+    console.log(value);
+})
